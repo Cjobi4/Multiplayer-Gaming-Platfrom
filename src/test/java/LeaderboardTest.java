d62cb859b4c1ed400762b2adc86b8d467cd2e2cc
Jordan Tran <jordan.tran2@ucalgary.ca>
2026-03-13 11:23:18 -0600

    correct branch for creating tests

--- src/test/java/LeaderboardTest.java
+++ src/test/java/LeaderboardTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+// We could initialize a mock leaderboard with a @BeforeEach, but we may need to create specific leaderboard for edge cases
+
 public class LeaderboardTest {
 }
