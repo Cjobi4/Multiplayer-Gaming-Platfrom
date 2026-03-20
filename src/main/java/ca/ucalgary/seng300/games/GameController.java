06b88e43292593bc1a91980516c6bb3c11b72f14
jonathan.hooi <jonathan.hooi@ucalgary.ca>
2026-03-19 20:53:43 -0600

    Added baseline GameController class with description and Constructor method stub of class to games package of main

---
+++ src/main/java/ca/ucalgary/seng300/games/GameController.java
@@ -0,0 +1,21 @@
+package ca.ucalgary.seng300.games;
+
+/**
+ * Game Controller Class
+ * <p>
+ *     Controller Class for Games, handling game-respective movement placement calls, turn
+ *     switching between (in)active players, and game termination logic.
+ * </p>
+ * @author Jonathan Hooi
+ * @email jonathan.hooi@ucalgary.ca
+ * @version 0.1 03/19/2026
+ * @implNote Construction of this Class made with respect to Player Profiles Stats Persistence
+ * Diagram from Project Iteration 1
+ */
+public class GameController {
+    //TODO: Constructor method stub
+    GameController() {
+
+    }
+
+}
