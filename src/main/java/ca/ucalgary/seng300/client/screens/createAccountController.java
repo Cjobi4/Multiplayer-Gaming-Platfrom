d1f84f82416a0d1345cfb4b5e8f297af6010a50e
gloreb <rebecca.glover@ucalgary.ca>
2026-03-22 15:02:25 -0600

    [client] Creating back buttons and populating createAccountPage.fxml (#105)
    Login page and Create Account page now have back buttons that bring the user back to the welcome page. createAccountPage.fxml also has text fields for username and password, as-well as an error field which we can use later to send error messages back to the user if their password is too long or if their username has already been taken.

--- src/main/java/ca/ucalgary/seng300/client/screens/createAccountController.java
+++ src/main/java/ca/ucalgary/seng300/client/screens/createAccountController.java
@@ -1,4 +1,39 @@
 package ca.ucalgary.seng300.client.screens;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class createAccountController {
+    public Button backButton;
+
+    @FXML
+    protected void onBackButtonClick(ActionEvent event) {
+        try {
+            //Load fxml file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/welcomePage.fxml"));
+            Parent welcomeRoot = loader.load();
+
+            //Get current stage from the button click
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            //Create new scene and set it on the stage
+            Scene welcomeScene = new Scene(welcomeRoot, 600, 400);
+            stage.setScene(welcomeScene);
+            stage.setTitle("Welcome!"); //Change stage title to reflect current scene
+            stage.show();
+
+        } catch (IOException e) {
+            System.err.println("Error: Could not load welcomePage.fxml. Check file path!");
+
+        }
+
+    }
 }
--- src/main/java/ca/ucalgary/seng300/client/screens/loginController.java
+++ src/main/java/ca/ucalgary/seng300/client/screens/loginController.java
@@ -15,6 +15,7 @@ public class loginController {
 
     public Button loginButton;
     public Button createAccountButton;
+    public Button backButton;
 
     @FXML
     protected void onLoginButtonClick(ActionEvent event) {
@@ -56,6 +57,29 @@ public class loginController {
 
         } catch (IOException e) {
             System.err.println("Error: Could not load createAccountPage.fxml. Check file path!");
+
+        }
+
+    }
+
+    @FXML
+    protected void onBackButtonClick(ActionEvent event) {
+        try {
+            //Load fxml file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/welcomePage.fxml"));
+            Parent welcomeRoot = loader.load();
+
+            //Get current stage from the button click
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            //Create new scene and set it on the stage
+            Scene welcomeScene = new Scene(welcomeRoot, 600, 400);
+            stage.setScene(welcomeScene);
+            stage.setTitle("Welcome!"); //Change stage title to reflect current scene
+            stage.show();
+
+        } catch (IOException e) {
+            System.err.println("Error: Could not load welcomePage.fxml. Check file path!");
 
         }
 
