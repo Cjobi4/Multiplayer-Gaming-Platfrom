9ce5dc111e28120ecef934b0dd25ef835523427d
gloreb <rebecca.glover@ucalgary.ca>
2026-03-22 15:28:20 -0600

    [client] Implementing "Back" Button (Game -> login) (#110)
    A new "back" button now allows the user to return to the login page from the game page.

--- src/main/java/ca/ucalgary/seng300/client/screens/mainController.java
+++ src/main/java/ca/ucalgary/seng300/client/screens/mainController.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 public class mainController {
 
     public Button gameSelectButton;
+    public Button backButton;
 
     @FXML
     protected void onGameSelectButtonClick(ActionEvent event) {
@@ -36,4 +37,26 @@ public class mainController {
         }
 
     }
-}
+
+    @FXML
+    protected void onBackButtonClick(ActionEvent event) {
+        try {
+            //Load fxml file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/loginPage.fxml"));
+            Parent loginRoot = loader.load();
+
+            //Get current stage from the button click
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            //Create new scene and set it on the stage
+            Scene loginScene = new Scene(loginRoot, 600, 400);
+            stage.setScene(loginScene);
+            stage.setTitle("Login Screen"); //Change stage title to reflect current scene
+            stage.show();
+
+        } catch (IOException e) {
+            System.err.println("Error: Could not load loginPage.fxml. Check file path!");
+
+        }
+
+    }}
