b34b1c965eacaa8ee7aa04fb30f69c4feb3fed42
gloreb <rebecca.glover@ucalgary.ca>
2026-03-22 15:07:48 -0600

    [client] Implementing "create account and continue button" (#105)
    createAccountPage.fxml "create account and continue" button now brings the user to the MainPage.fxml. Currently, verification steps are not in place. The button changes the stage without any verification checks.

--- src/main/java/ca/ucalgary/seng300/client/screens/createAccountController.java
+++ src/main/java/ca/ucalgary/seng300/client/screens/createAccountController.java
@@ -13,6 +13,31 @@ import java.io.IOException;
 
 public class createAccountController {
     public Button backButton;
+    public Button continueButton;
+
+    @FXML
+    protected void onContinueButtonClick(ActionEvent event) {
+        try {
+            //Load fxml file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/mainPage.fxml"));
+            Parent MainRoot = loader.load();
+
+            //Get current stage from the button click
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            //Create new scene and set it on the stage
+            Scene MainScene = new Scene(MainRoot, 800, 600);
+            stage.setScene(MainScene);
+            stage.setTitle("Main Page"); //Change stage title to reflect current scene
+            stage.show();
+
+        } catch (IOException e) {
+            System.err.println("Error: Could not load mainPage.fxml. Check file path!");
+        }
+
+    }
+
+
 
     @FXML
     protected void onBackButtonClick(ActionEvent event) {
