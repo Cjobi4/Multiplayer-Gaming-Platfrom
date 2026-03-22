d06fb7dd8f4eaa3aab65e06186d4e10172af973e
gloreb <rebecca.glover@ucalgary.ca>
2026-03-22 14:41:41 -0600

    [client] Creating Buttons to create account (#105)
    Login page and welcome page each now have a button that navigates the user to the create-account page. createAccountPage.fxml is still very blank.

--- src/main/java/ca/ucalgary/seng300/client/screens/loginController.java
+++ src/main/java/ca/ucalgary/seng300/client/screens/loginController.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 public class loginController {
 
     public Button loginButton;
+    public Button createAccountButton;
 
     @FXML
     protected void onLoginButtonClick(ActionEvent event) {
@@ -33,6 +34,29 @@ public class loginController {
 
         } catch (IOException e) {
             System.err.println("Error: Could not load mainPage.fxml. Check file path!");
+        }
+
+    }
+
+    @FXML
+    protected void onCreateAccountButtonClick(ActionEvent event) {
+        try {
+            //Load fxml file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/createAccountPage.fxml"));
+            Parent createAccountRoot = loader.load();
+
+            //Get current stage from the button click
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            //Create new scene and set it on the stage
+            Scene createAccountScene = new Scene(createAccountRoot, 600, 400);
+            stage.setScene(createAccountScene);
+            stage.setTitle("Create Account"); //Change stage title to reflect current scene
+            stage.show();
+
+        } catch (IOException e) {
+            System.err.println("Error: Could not load createAccountPage.fxml. Check file path!");
+
         }
 
     }
