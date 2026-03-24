c90175860b1e534ac721798bfa43ce800a058bbc
Jaspreet Sandhu <jaspreet.sandhu3@ucalgary.ca>
2026-03-24 08:58:48 -0600

    [Quality Testing] Created test outlines (90)

--- src/test/java/ca/ucalgary/seng300/games/ConnectFourTest.java
+++ src/test/java/ca/ucalgary/seng300/games/ConnectFourTest.java
@@ -1,4 +1,105 @@
 package ca.ucalgary.seng300.games;
 
+import ca.ucalgary.seng300.games.connectfour.*;
+import org.junit.jupiter.api.Test;
+
 public class ConnectFourTest {
+    // test one: is the 6x7 board being created
+    @Test
+    void testConnectFourBoardSetsUp6x7() {
+        // 1. arrange
+
+        // 2. act
+
+        // 3. assertions
+    }
+
+    @Test
+    void testConnectFourVerticalLeftMostEdgeWinsCorrect(){
+        // 1. arrange
+
+        // 2. act
+
+        // 3. assertions
+    }
+    @Test
+    void testConnectFourVerticalRightMostEdgeWinsCorrect(){
+        // 1. arrange
+
+        // 2. act
+
+        // 3. assertions
+    }
+    @Test
+    void testBackslashDiagonalWinsCorrect(){
+        // 1. arrange
+
+        // 2. act
+
+        // 3. assertions
+    }
+
+    @Test
+    void testForwardslashDiagonalWinsCorrect(){
+        // 1. arrange
+
+        // 2. act
+
+        // 3. assertions
+    }
+
+    @Test
+    void testHorizontal1stRowWinsCorrect(){
+        // 1. arrange
+
+        // 2. act
+
+        // 3. assertions
+    }
+
+    @Test
+    void testHorizontal2ndRowWinsCorrect(){
+        // 1. arrange
+
+        // 2. act
+
+        // 3. assertions
+    }
+
+    @Test
+    void testHorizontal3rdRowWinsCorrect(){
+        // 1. arrange
+
+        // 2. act
+
+        // 3. assertions
+    }
+
+    @Test
+    void testHorizontal4thRowWinsCorrect(){
+        // 1. arrange
+
+        // 2. act
+
+        // 3. assertions
+    }
+
+    @Test
+    void testHorizontal5thRowWinsCorrect(){
+        // 1. arrange
+
+        // 2. act
+
+        // 3. assertions
+    }
+
+    @Test
+    void testHorizontal6thRowWinsCorrect(){
+        // 1. arrange
+
+        // 2. act
+
+        // 3. assertions
+    }
+
 }
