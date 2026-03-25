a7fa3e44b8bfd9065b9d27f1ee90c3b6d4a7acc7
Anh Nguyen <anh.nguyen10@ucalgary.ca>
2026-03-25 00:59:50 -0600

    [Platform] Added toString method for GameType class (#49)

--- src/main/java/ca/ucalgary/seng300/rules/leaderboard/GameType.java
+++ src/main/java/ca/ucalgary/seng300/rules/leaderboard/GameType.java
@@ -1,5 +1,15 @@
 package ca.ucalgary.seng300.rules.leaderboard;
 
+
 public enum GameType {
-    TICTACTOE, CONNECT4, COMBINED;
+    TICTACTOE ("Tic-Tac-Toe"), CONNECT4 ("Connect 4");
+    private final String name;
+
+    GameType (String name){
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
