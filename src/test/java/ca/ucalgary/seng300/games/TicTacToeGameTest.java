5256e6195c147960a26b9487b2e6ed231975fbfb
Jordan Tran <jordan.tran2@ucalgary.ca>
2026-03-26 16:31:27 -0600

    Created feature branch for tic tac toe testing #91

--- src/test/java/ca/ucalgary/seng300/games/TicTacToeGameTest.java
+++ src/test/java/ca/ucalgary/seng300/games/TicTacToeGameTest.java
@@ -1,4 +1,14 @@
 package ca.ucalgary.seng300.games;
 
+import ca.ucalgary.seng300.games.tictactoe.TicTacToeBoard;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class TicTacToeGameTest {
+
+    @BeforeAll
+    static void setUp(){
+        TicTacToeBoard testBoard = new TicTacToeBoard();
+    }
+
 }
