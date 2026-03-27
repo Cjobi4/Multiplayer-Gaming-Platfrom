5f16f9decdfa98be5b8f782f7de189134afe0692
Jaspreet Sandhu <jaspreet.sandhu3@ucalgary.ca>
2026-03-26 20:29:50 -0600

    [Quality Testing] Added the setup for all chat registry test cases (159)

--- src/test/java/ca/ucalgary/seng300/core/ChatRegistryTest.java
+++ src/test/java/ca/ucalgary/seng300/core/ChatRegistryTest.java
@@ -1,4 +1,17 @@
 package ca.ucalgary.seng300.core;
 
+import ca.ucalgary.seng300.core.registry.ChatRegistry;
+import ca.ucalgary.seng300.shared.models.Message;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class ChatRegistryTest {
+    private ChatRegistry chatRegistry;
+    private static Message[] messages;
+
+    @BeforeEach
+    public void setup(){
+        chatRegistry = ChatRegistry.getInstance();
+        chatRegistry.ListAll().clear();
+    }
 }
