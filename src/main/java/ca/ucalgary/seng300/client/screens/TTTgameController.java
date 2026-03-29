df6e966997d5ad8b70c87dbc4913e21ba86c26af
gloreb <rebecca.glover@ucalgary.ca>
2026-03-29 16:00:59 -0600

    [client] Creating new TTT files (#182)
    Created TTTgameController.java, and TTTgamePage.fxml. These pages are now connected to the TTTopponentSelectController.

---
+++ src/main/java/ca/ucalgary/seng300/client/screens/TTTgameController.java
@@ -0,0 +1,67 @@
+package ca.ucalgary.seng300.client.screens;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TTTgameController {
+    public Button gameOverButton;
+    public Button backButton;
+
+    @FXML
+    protected void onGameOverButtonClick(ActionEvent event) {
+        try {
+            //Load fxml file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/gameOverDisplay.fxml"));
+            Parent gameOverRoot = loader.load();
+
+            //Get current stage from the button click
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            //Create new scene and set it on the stage
+            Scene gameOverScene = new Scene(gameOverRoot, 800, 600);
+            stage.setScene(gameOverScene);
+            stage.setTitle("Game Over"); //Change stage title to reflect current scene
+            stage.show();
+
+            // Create Match Record (Not sure where to get player info, will be needed to make a match record)
+
+            // MatchRecord matchRecord = new MatchRecord();
+
+            // Update Leaderboard
+
+            // TODO Need to communicate with someone on my team for this part, their code is hard for me to understand
+
+        } catch (IOException e) {
+            System.err.println("Error: Could not load gameOverDisplay.fxml. Check file path!");
+        }
+    }
+
+    @FXML
+    protected void onBackButtonClick(ActionEvent event) {
+        try {
+            //Load fxml file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/TTTopponentSelectPage.fxml"));
+            Parent opponentRoot = loader.load();
+
+            //Get current stage from the button click
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            //Create new scene and set it on the stage
+            Scene opponentScene = new Scene(opponentRoot, 800, 600);
+            stage.setScene(opponentScene);
+            stage.setTitle("Tic-Tac-Toe - Opponent Select"); //Change stage title to reflect current scene
+            stage.show();
+
+        } catch (IOException e) {
+            System.err.println("Error: Could not load TTTopponentSelectPage.fxml. Check file path!");
+        }
+    }
+}
--- src/main/java/ca/ucalgary/seng300/client/screens/TTTopponentSelectController.java
+++ src/main/java/ca/ucalgary/seng300/client/screens/TTTopponentSelectController.java
@@ -19,7 +19,7 @@ public class TTTopponentSelectController {
     protected void onOpponentSelectedButtonClick(ActionEvent event) {
         try {
             //Load fxml file
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/gamePage.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/TTTgamePage.fxml"));
             Parent gameRoot = loader.load();
 
             //Get current stage from the button click
@@ -28,11 +28,11 @@ public class TTTopponentSelectController {
             //Create new scene and set it on the stage
             Scene gameScene = new Scene(gameRoot, 800, 600);
             stage.setScene(gameScene);
-            stage.setTitle("Game"); //Change stage title to reflect current scene
+            stage.setTitle("Tic-Tac-Toe - Game"); //Change stage title to reflect current scene
             stage.show();
 
         } catch (IOException e) {
-            System.err.println("Error: Could not load gamePage.fxml. Check file path!");
+            System.err.println("Error: Could not load TTTgamePage.fxml. Check file path!");
         }
     }
 
