6326e8cae542b0f68af38d2fac284fbf0b0470ca
Owen <owen.hilton@ucalgary.ca>
2026-03-31 14:58:47 -0600

    Finished functions for player registry #208

--- src/main/java/ca/ucalgary/seng300/shared/models/Player.java
+++ src/main/java/ca/ucalgary/seng300/shared/models/Player.java
@@ -1,4 +1,26 @@
 package ca.ucalgary.seng300.shared.models;
 
 public class Player {
+
+    private final String id;
+    private final String name;
+
+
+    public Player(String id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+
 }
