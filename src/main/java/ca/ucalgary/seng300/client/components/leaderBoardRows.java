eabe329b194140e3129ceb72f46e92dba773b712
Cj Obi <cj.obi@ucalgary.ca>
2026-03-31 12:11:01 -0600

    [Client] Creating a sample class with getters(#188)
    This will allow me to get the users info

--- src/main/java/ca/ucalgary/seng300/client/components/leaderBoardRows.java
+++ src/main/java/ca/ucalgary/seng300/client/components/leaderBoardRows.java
@@ -1,5 +1,31 @@
 package ca.ucalgary.seng300.client.components;
 
 public class leaderBoardRows {
+    private final int rank;
+    private final String playerName;
+    private final int wins;
+    private final int matches;
 
+    public leaderBoardRows(int rank, String playerName, int wins, int matches) {
+        this.rank = rank;
+        this.playerName = playerName;
+        this.wins = wins;
+        this.matches = matches;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public int getMatches() {
+        return matches;
+    }
 }
