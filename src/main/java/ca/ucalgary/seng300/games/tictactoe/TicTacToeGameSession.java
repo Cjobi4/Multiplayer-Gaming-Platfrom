e6fb1ed3b6fa23ca6e0d88f9f48485d662fdd6d7
sajanjohal <sajan.johal@ucalgary.ca>
2026-04-01 20:15:14 -0600

    created session variables for player session management (ticket #232)

--- src/main/java/ca/ucalgary/seng300/games/tictactoe/TicTacToeGameSession.java
+++ src/main/java/ca/ucalgary/seng300/games/tictactoe/TicTacToeGameSession.java
@@ -1,4 +1,11 @@
 package ca.ucalgary.seng300.games.tictactoe;
 
-public class TicTacToeGameSession {
+import ca.ucalgary.seng300.core.identity.client.Session;
+
+public class TicTacToeGameSession extends Thread{
+    private Session playerOneSession;
+    private Session playerTwoSession;
+    private TicTacToeGame game;
+
+
 }
