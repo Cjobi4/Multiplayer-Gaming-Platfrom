0ed22784a86bdc8fce06b96f8bf142deccb83926
Jaspreet Sandhu <jaspreet.sandhu3@ucalgary.ca>
2026-04-02 10:16:11 -0600

    [Quality Testing] Skeleton test cases created for player registry (216)

--- src/test/java/ca/ucalgary/seng300/core/PlayerRegistryTest.java
+++ src/test/java/ca/ucalgary/seng300/core/PlayerRegistryTest.java
@@ -1,4 +1,64 @@
 package ca.ucalgary.seng300.core;
 
+import ca.ucalgary.seng300.core.registry.GameRegistry;
+import ca.ucalgary.seng300.core.registry.PlayerRegistry;
+import ca.ucalgary.seng300.shared.models.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
 public class PlayerRegistryTest {
+    private PlayerRegistry registry;
+
+    @BeforeEach
+    public void setUp(){
+        registry = PlayerRegistry.getInstance();
+        registry.listAll().clear();
+    }
+
+
+    @Test
+    void testPlayerRegistryInstanceReturnsNotNull() {
+        PlayerRegistry instance = PlayerRegistry.getInstance();
+
+        assertNotNull(instance, "Player registry should not be null.");
+    }
+
+
+    @Test
+    void testPlayerRegistryFindByIdReturnsPlayer() {
+
+    }
+
+
+    @Test
+    void testPlayerRegistryFindByIdReturnsNull() {
+
+    }
+
+
+    @Test
+    void testPlayerRegistryFindByNameReturnsPlayer() {
+
+    }
+
+
+    @Test
+    void testPlayerRegistryFindByNameReturnsNull() {
+
+    }
+
+
+    @Test
+    void testPlayerRegistryListAllReturnsPlayers() {
+
+    }
+
+
+    @Test
+    void testPlayerRegistryListAllReturnsNull() {
+
+    }
 }
