0b76bea3c180de23cdb706329a9e86dfc9361f65
Jaspreet Sandhu <jaspreet.sandhu3@ucalgary.ca>
2026-04-01 18:04:41 -0600

    [Quality Testing] Added javadocs and altered test testFindByIdReturnsCorrectGameID (163)

--- src/test/java/ca/ucalgary/seng300/core/GameRegistryTest.java
+++ src/test/java/ca/ucalgary/seng300/core/GameRegistryTest.java
@@ -14,19 +14,31 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- *
+ * This is a test class for the Game Registry Class
  */
 public class GameRegistryTest {
 
     private GameRegistry gameRegistry;
     private static Game game;
 
+    /**
+     * This uses a single instance of the game registry
+     * Clear the game registry before each test
+     * Cleared registry ensures that there are no conflicts
+     */
     @BeforeEach
     void setUp() {
         gameRegistry = GameRegistry.getInstance();
         gameRegistry.ListAll().clear();
     }
 
+    /**
+     * This test tests to see if a single game can correctly be added to the game registry
+     * Input: adding game1 with id of "game1" into the registry
+     * Expected Output 1: Game registry should have a size of 1
+     * Expected Output 2: ID of the game in the game registry should be "game1"
+     * Actual Outputs: Actual size of game registry and ID of the game in the registry
+     */
     @Test
     void testRegisterGameAddsSingleGame() {
         List<Tag> tags = Collections.singletonList(new Tag("two-player", "RED"));
@@ -45,20 +57,31 @@ public class GameRegistryTest {
         assertEquals(expectedID, actualID, "Expected ID is: " + expectedID + ", but was: " + actualID);
     }
 
+    /**
+     * Testing the method findById()
+     * Input: ID of game1, which is set as "game1"
+     * Expected Output: game1
+     * Actual Output: the game found using the ID "game1"
+     */
     @Test
     void testFindByIdReturnsCorrectGameID() {
         List<Tag> tags = Collections.singletonList(new Tag("two-player", "RED"));
         LeaderBoard leaderBoard = null;
         Game game1 = new Game("game1", "CONNECT4", "some_description", tags, leaderBoard);
         gameRegistry.register(game1);
-        String expectedID = "game1";
+        Game expectedGame = game1;
 
-        Game foundGame = gameRegistry.findById("game1");
-        String actualId = foundGame.getId();
+        // this (.findById()) is the method that we are testing
+        Game actualGame = gameRegistry.findById("game1");
 
-        assertEquals(expectedID, actualId, "Expected ID for the game is:" + expectedID + " and found: " + actualId);
+        assertEquals(expectedGame, actualGame, "Expected ID for the game is:" + expectedGame + " and found: " + actualGame);
     }
 
+    /**
+     * Testing the method findById() when the ID is not related to a game in the registry
+     * Input: ID set as "does_not_exist"
+     * Expected output: null
+     */
     @Test
     void testFindByIdWhenGameDNE() {
         Game found = gameRegistry.findById("does_not_exist");
@@ -66,6 +89,12 @@ public class GameRegistryTest {
         assertNull(found, "No game should have been found.");
     }
 
+    /**
+     * This test tests to see if multiple games can correctly be added to the game registry
+     * Input: game1, game2, game3
+     * Expected Output: size of 3
+     * Actual Output: size of actual game registry
+     */
     @Test
     void testAddMultipleGameReturnsCorrectRegistrySize() {
         List<Tag> tags = Collections.singletonList(new Tag("two-player", "RED"));
@@ -84,6 +113,10 @@ public class GameRegistryTest {
         assertEquals(expectedSize, gameRegistry.ListAll().size(), "Game Registry size should be equal to expected size of: " + expectedSize);
     }
 
+    /**
+     * Testing the getInstance() method
+     * Expected Output: an existing instance, should never be null even if one didn't exist before
+     */
     @Test
     void testGetInstanceCorrectly() {
         GameRegistry instance = GameRegistry.getInstance();
@@ -91,6 +124,11 @@ public class GameRegistryTest {
         assertNotNull(instance, "Game Registry instance should not be null");
     }
 
+    /**
+     * Inputs: game1, game2, game3
+     * Expected Outputs: IDs of "game1", "game2", "game3"
+     * Actual Outputs: the IDs of the games stored in game registry
+     */
     @Test
     void testListAllReturnsAllGamesByIndex() {
         List<Tag> tags = Collections.singletonList(new Tag("two-player", "RED"));
