f1d2094b0986b9703d4815476fec8ab9468c62c1
Jaspreet Sandhu <jaspreet.sandhu3@ucalgary.ca>
2026-04-02 15:47:14 -0600

    [Quality Testing] Created tests for Connect 4 board, in progress (90)

--- src/test/java/ca/ucalgary/seng300/games/ConnectFourBoardTest.java
+++ src/test/java/ca/ucalgary/seng300/games/ConnectFourBoardTest.java
@@ -3,7 +3,7 @@ package ca.ucalgary.seng300.games;
 import ca.ucalgary.seng300.games.connectfour.ConnectFourBoard;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ConnectFourBoardTest {
     @Test
@@ -29,25 +29,104 @@ public class ConnectFourBoardTest {
     @Test
     void testDropPieceWhenColumnIsEmpty() {
         ConnectFourBoard board = new ConnectFourBoard();
+        char player = 'X';
+        board.dropPiece(5, player);
+
+        char expectedPiece = 'X';
+        char actualPiece = board.getCell(5, 5);
+
+        assertTrue(board.dropPiece(5, player), "The board is empty, thus the piece should be dropped.");
+        assertEquals(expectedPiece, actualPiece, "The piece should be dropped and should show as the player.");
     }
 
     @Test
     void testDropPieceWhenColumnIsFull() {
         ConnectFourBoard board = new ConnectFourBoard();
+        char player1 = 'X';
+        char player2 = 'O';
+
+        //filling the board with player 'O'
+        for (int row = 0; row < 6; row++) {
+            for (int col = 0; col < 7; col++) {
+                board.dropPiece(col, player2);
+            }
+        }
+
+        // now, check if player X's piece can be dropped when the column/board is full
+        boolean expectedReturn = false;
+        boolean actualReturn = board.dropPiece(6, player1);
+
+        assertEquals(expectedReturn, actualReturn, "Should not be able to add player X's piece when the column/board is full");
+    }
+
+    @Test
+    void testDropPieceWhenOutOfBounds() {
+        ConnectFourBoard board = new ConnectFourBoard();
+        char player = 'X';
+
+        assertFalse(board.dropPiece(7, player), "The chosen column is out of bounds, thus the method dropPiece(col, player) should return as false.");
     }
 
     @Test
     void testIsFullWhenBoardIsEmpty() {
         ConnectFourBoard board = new ConnectFourBoard();
+        boolean expectedReturn = false;
+        boolean actualReturn = board.isFull();
+
+        assertEquals(expectedReturn, actualReturn, "The board should not be full.");
     }
 
     @Test
     void testIsFullWhenBoardIsNeitherEmptyNorFull() {
         ConnectFourBoard board = new ConnectFourBoard();
+        char player1 = 'X';
+        char player2 = 'O';
+
+        // filling one column completely but leaving the rest with just a few pieces
+        board.dropPiece(0, player1);
+        board.dropPiece(0, player2);
+        board.dropPiece(0, player1);
+        board.dropPiece(0, player2);
+        board.dropPiece(0, player1);
+        board.dropPiece(0, player2);
+
+        board.dropPiece(1, player1);
+        board.dropPiece(1, player2);
+
+        board.dropPiece(2, player1);
+        board.dropPiece(2, player2);
+
+        board.dropPiece(3, player1);
+
+        board.dropPiece(4, player2);
+
+        board.dropPiece(5, player1);
+        board.dropPiece(5, player2);
+
+        board.dropPiece(6, player1);
+        board.dropPiece(6, player2);
+
+        // the actual test
+        boolean expectedReturn = false;
+        boolean actualReturn = board.isFull();
+        assertEquals(expectedReturn, actualReturn, "The board should not be full.");
     }
 
     @Test
     void testIsFullWhenBoardIsFull() {
         ConnectFourBoard board = new ConnectFourBoard();
+        char player = 'O';
+
+        //filling the board with player 'O'
+        for (int row = 0; row < 6; row++) {
+            for (int col = 0; col < 7; col++) {
+                board.dropPiece(col, player);
+            }
+        }
+
+        boolean expectedReturn = true;
+        boolean actualReturn = board.isFull();
+
+        assertEquals(expectedReturn, actualReturn, "The board should be full.");
     }
 }
