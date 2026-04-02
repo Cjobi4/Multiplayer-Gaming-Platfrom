f981277b57296185b8b320822780f2c7d3eb3b69
Jaspreet Sandhu <jaspreet.sandhu3@ucalgary.ca>
2026-04-02 10:39:52 -0600

    [Quality Testing] Player Registry test cases created and tested, all working (216)

--- src/test/java/ca/ucalgary/seng300/core/PlayerRegistryTest.java
+++ src/test/java/ca/ucalgary/seng300/core/PlayerRegistryTest.java
@@ -6,6 +6,11 @@ import ca.ucalgary.seng300.shared.models.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
@@ -29,24 +34,68 @@ public class PlayerRegistryTest {
 
     @Test
     void testPlayerRegistryFindByNameReturnsPlayer() {
+        Player testPlayer = new Player("jane doe");
+        registry.register(testPlayer);
+        Player expectedPlayer = testPlayer;
 
+        Player actualPlayerFound = registry.findByName(testPlayer.getName());
+
+        assertEquals(expectedPlayer, actualPlayerFound, "Player found by findByName method should be: " + expectedPlayer + ", actual player found is: " + actualPlayerFound);
     }
 
 
     @Test
     void testPlayerRegistryFindByNameReturnsNull() {
+        // making sure that the registry isn't empty
+        Player testPlayer = new Player("jane doe");
+        registry.register(testPlayer);
 
+        Player expectedPlayer = null;
+        Player actualPlayerFound = registry.findByName("john doe"); // this is a name that is not registered in the game registry
+
+        assertEquals(expectedPlayer, actualPlayerFound, "Player found by findByName method should be null.");
+    }
+
+
+    @Test
+    void testPlayerRegistryFindByIdReturnsNullWhenPlayerExistsButNotRegistered() {
+        Player testPlayer = new Player("jane doe");
+        registry.register(testPlayer);
+        Player testPlayer2 = new Player("john doe"); // this player will not be registered into the registry
+
+        Player expectedPlayer = null;
+        Player actualPlayerFound = registry.findByName("john doe"); // this is a name that is not registered in the game registry
+
+        assertEquals(expectedPlayer, actualPlayerFound, "Player found by findByName method should be null.");
     }
 
 
     @Test
     void testPlayerRegistryListAllReturnsPlayers() {
+        Player testPlayer = new Player("jane doe");
+        registry.register(testPlayer);
+        Player testPlayer2 = new Player("john doe");
+        registry.register(testPlayer2);
+        Player testPlayer3 = new Player("john suh");
+        registry.register(testPlayer3);
 
+        List<Player> expectedPlayers = Arrays.asList(testPlayer, testPlayer2, testPlayer3);
+        List<Player> actualPlayers = registry.listAll();
+
+        assertEquals(expectedPlayers, actualPlayers, "Player list should be equal.");
     }
 
 
     @Test
     void testPlayerRegistryListAllReturnsNull() {
+        // these are players that aren't added to the registry, thus the registry should be empty
+        Player testPlayer = new Player("jane doe");
+        Player testPlayer2 = new Player("john doe");
+        Player testPlayer3 = new Player("john suh");
 
+        List<Player> expectedPlayers = new ArrayList<>(); // empty
+        List<Player> actualPlayers = registry.listAll();
+
+        assertEquals(expectedPlayers, actualPlayers, "Player list should be empty.");
     }
 }
