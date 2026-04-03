5a79a300c5458472bc91d9252e04fbd4489c4caf
Jaspreet Sandhu <jaspreet.sandhu3@ucalgary.ca>
2026-04-02 22:57:49 -0600

    [Quality Testing] Altered test name and filling tests, in progress (90)

--- src/test/java/ca/ucalgary/seng300/games/ConnectFourGameTest.java
+++ src/test/java/ca/ucalgary/seng300/games/ConnectFourGameTest.java
@@ -1,16 +1,39 @@
 package ca.ucalgary.seng300.games;
 
+import ca.ucalgary.seng300.games.connectfour.ConnectFourGame;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ConnectFourGameTest {
     @Test
-    void testConnectFourVerticalLeftMostEdgeWinsCorrect(){
-        // 1. arrange
+    void testGetCurrentPlayerBeforeMoveMade() {
+        ConnectFourGame game = new ConnectFourGame();
+        char expectedCurrentPlayer = 'X';
+        char actualCurrentPlayer = game.getCurrentPlayer();
 
-        // 2. act
-
-        // 3. assertions
+        assertEquals(expectedCurrentPlayer, actualCurrentPlayer, "The currently player is expected to be 'X' and the actual current player is: " + actualCurrentPlayer);
     }
+
+    @Test
+    void testConnectFourVertical0thColumnWinsCorrect(){
+        ConnectFourGame game = new ConnectFourGame();
+        char player1 =  'X';
+        char player2 = 'O';
+
+        game.makeMove(0);   // player x 1
+        game.makeMove(1);   // player o
+        game.makeMove(0);   // player x 2
+        game.makeMove(2);   // player o
+        game.makeMove(0);   // player x 3
+        game.makeMove(2);   // player o
+        game.makeMove(0);   // player x
+
+        boolean expectedWinCondition = true;
+        boolean actualWinCondition = game.checkWin();
+        assertEquals(expectedWinCondition, actualWinCondition, "The win condition should be true for the 0 column vertical.");
+    }
+
     @Test
     void testConnectFourVerticalRightMostEdgeWinsCorrect(){
         // 1. arrange
@@ -19,6 +42,7 @@ public class ConnectFourGameTest {
 
         // 3. assertions
     }
+
     @Test
     void testBackslashDiagonalWinsCorrect(){
         // 1. arrange
@@ -29,7 +53,7 @@ public class ConnectFourGameTest {
     }
 
     @Test
-    void testForwardslashDiagonalWinsCorrect(){
+    void testForwardSlashDiagonalWinsCorrect(){
         // 1. arrange
 
         // 2. act
