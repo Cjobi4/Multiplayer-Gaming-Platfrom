220b31726463cd3aa23b84467e17563eb9d0c5d8
gloreb <rebecca.glover@ucalgary.ca>
2026-04-02 22:12:10 -0600

    [client] Remove "procede to Game Over Page" button (#200)

--- src/main/java/ca/ucalgary/seng300/client/screens/TTTgameController.java
+++ src/main/java/ca/ucalgary/seng300/client/screens/TTTgameController.java
@@ -23,7 +23,6 @@ import ca.ucalgary.seng300.games.GameState;
 import java.io.IOException;
 
 public class TTTgameController {
-    public Button gameOverButton;
     public Button backButton;
     public Button ttt00;
     public Button ttt01;
@@ -78,45 +77,13 @@ public class TTTgameController {
         chatScrollPane.setVvalue(1.0);
     }
 
-    @FXML
-    protected void onGameOverButtonClick(ActionEvent event) {
-        try {
-            //Load fxml file
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/gameOverDisplay.fxml"));
-            Parent gameOverRoot = loader.load();
-
-            //Get current stage from the button click
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-
-            //Create new scene and set it on the stage
-            Scene gameOverScene = new Scene(gameOverRoot, 800, 600);
-            stage.setScene(gameOverScene);
-            stage.setTitle("Game Over"); //Change stage title to reflect current scene
-            stage.show();
-
-            // Create Match Record (Not sure where to get player info, will be needed to make a match record)
-
-            // MatchRecord matchRecord = new MatchRecord();
-
-            // Update Leaderboard
-
-            // TODO Need to communicate with someone on my team for this part, their code is hard for me to understand
-
-        } catch (IOException e) {
-            System.err.println("Error: Could not load gameOverDisplay.fxml. Check file path!");
-        }
-    }
 
     protected void gameOver(){ //copy of the button version
         try {
             //Load fxml file
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/gameOverDisplay.fxml"));
             Parent gameOverRoot = loader.load();
-
-            //Get current stage from the button click
             Stage stage = (Stage) ttt00.getScene().getWindow();
-
-            //Create new scene and set it on the stage
             Scene gameOverScene = new Scene(gameOverRoot, 800, 600);
             stage.setScene(gameOverScene);
             stage.setTitle("Game Over"); //Change stage title to reflect current scene
