1065456842265c1ed99a384c24dc5c38a5437497
Jaspreet Sandhu <jaspreet.sandhu3@ucalgary.ca>
2026-04-03 00:10:14 -0600

    [Quality Testing] New BeforeEach and AfterEach to check print statements (90)

--- src/test/java/ca/ucalgary/seng300/games/ConnectFourBoardTest.java
+++ src/test/java/ca/ucalgary/seng300/games/ConnectFourBoardTest.java
@@ -1,11 +1,29 @@
 package ca.ucalgary.seng300.games;
 
 import ca.ucalgary.seng300.games.connectfour.ConnectFourBoard;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ConnectFourBoardTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUpStreams(){
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams(){
+        System.setOut(originalOut);
+    }
+
     @Test
     void testConnectFourBoardSetsUp6x7() {
         ConnectFourBoard board = new ConnectFourBoard();
