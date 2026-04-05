e840820e27add491e62122764c73914302cd7053
Jordan Tran <jordan.tran2@ucalgary.ca>
2026-04-05 17:32:28 -0600

    Wrote a BeforeAll concerning connection to the server, mandatory for testing proper response. #203

--- src/test/java/ca/ucalgary/seng300/core/SessionTest.java
+++ src/test/java/ca/ucalgary/seng300/core/SessionTest.java
@@ -1,8 +1,32 @@
 package ca.ucalgary.seng300.core;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.net.Socket;
+
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
 public class SessionTest {
+
+    /*
+    Tests that require an active connection with the main server
+    For this reason, we cannot simply fake an intended output
+     */
+    @BeforeAll
+    static void establishConnection(){
+        boolean isServerAvailable = false; // assume we can't connect to the server
+
+        try(Socket testConnect = new Socket("127.0.0.1", 14001)){
+            isServerAvailable = true; // connection could be established
+        }catch(IOException e){
+            isServerAvailable = false; // connection could not be established
+        }
+
+        assumeTrue(isServerAvailable, "Tests could not be run, no connection to the server.");
+        // if no connection is made, the rest of the tests will be skipped as they WILL fail with no connection
+    }
 
     @Test
     void testStartSession(){
