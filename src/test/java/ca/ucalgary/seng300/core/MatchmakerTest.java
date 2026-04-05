61a0cd6c9e023ef77b698a9ffc6466aa91955b4d
Jaspreet Sandhu <jaspreet.sandhu3@ucalgary.ca>
2026-04-05 10:50:10 -0600

    Removed stub, figured out mistake (#202)

---
+++ src/test/java/ca/ucalgary/seng300/core/MatchmakerTest.java
@@ -0,0 +1,28 @@
+package ca.ucalgary.seng300.core;
+
+import org.junit.jupiter.api.Test;
+import ca.ucalgary.seng300.Matchmaker;
+
+class MatchmakerTest {
+
+    @Test
+    void run() {
+    }
+
+    @Test
+    void joinMQueueCorrectly () {
+    }
+
+    @Test
+    void joinMQueueThrowsException () {
+    }
+
+    @Test
+    void leaveMQueueCorrectly() {
+    }
+
+    @Test
+    void leaveMQueueThrowsException () {
+
+    }
+}
