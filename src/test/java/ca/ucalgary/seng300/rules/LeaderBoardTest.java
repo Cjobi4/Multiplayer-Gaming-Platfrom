f4d066451764ce98fda6dcf157d427b6c38a2233
Jaspreet Sandhu <jaspreet.sandhu3@ucalgary.ca>
2026-04-05 22:26:49 -0600

    Created LeaderBoardTest, in progress (#163)

---
+++ src/test/java/ca/ucalgary/seng300/rules/LeaderBoardTest.java
@@ -0,0 +1,20 @@
+package ca.ucalgary.seng300.rules;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeaderBoardTest {
+
+    @Test
+    void testGetLeaderboard() {
+    }
+
+    @Test
+    void testGetUserRecord() {
+    }
+
+    @Test
+    void testGetUserMatchRecords() {
+    }
+}
