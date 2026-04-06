612527955a4d8555f5cd698729f547b56ffa38f8
Jaspreet Sandhu <jaspreet.sandhu3@ucalgary.ca>
2026-04-05 22:21:41 -0600

    Completed Match Record tests (#164)

--- src/test/java/ca/ucalgary/seng300/rules/MatchRecordTest.java
+++ src/test/java/ca/ucalgary/seng300/rules/MatchRecordTest.java
@@ -2,15 +2,15 @@ package ca.ucalgary.seng300.rules;
 
 import ca.ucalgary.seng300.rules.leaderboard.GameType;
 import ca.ucalgary.seng300.rules.leaderboard.MatchRecord;
-import ca.ucalgary.seng300.shared.models.Game;
-import ca.ucalgary.seng300.shared.models.Tag;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
-import java.util.List;
+import java.time.ZonedDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * This test class is testing the methods from the MatchRecord class
+ */
 class MatchRecordTest {
 
     // initial set up
@@ -18,22 +18,55 @@ class MatchRecordTest {
     String username2 = "username2";
     GameType gameType1 = GameType.CONNECT4;
     String winner1 = "username1";
+    String fakeWinner =  "username2";
+    String date1 = "2019-05-05";
 
     MatchRecord matchRecord = new MatchRecord(username1, username2, gameType1, winner1);
 
+    /**
+     * Checking if the getter for isWinner is correctly running
+     */
     @Test
-    void testIsWinner() {
+    void testIsWinnerSuccessfully() {
+        boolean actualWinner = matchRecord.isWinner(winner1);
+        assertTrue(actualWinner, "The winner should be: " + winner1);
     }
 
+    /**
+     * Checking if the getter for isWinner is correctly running by returning false when fakeWinner is not the winner
+     */
+    @Test
+    void testIsWinnerReturnsFalse() {
+        boolean actualWinner = matchRecord.isWinner(fakeWinner);
+        assertFalse(actualWinner, "The boolean value should be false");
+    }
+
+    /**
+     * Checking if the getter for getGameType is correctly running
+     */
     @Test
     void testGetGameType() {
+        GameType actualGameType = matchRecord.getGameType();
+        assertEquals(gameType1, actualGameType, "The game type should be " + gameType1);
     }
 
+    /**
+     * Checking if the getter for getDate is correctly running
+     */
     @Test
     void testGetDate() {
+        // this test should fail
+        ZonedDateTime actualDate = ZonedDateTime.parse(matchRecord.getDate());
     }
 
+    /**
+     * Checking if toString is correctly running
+     */
     @Test
     void testToString() {
+        // this test should fail because of date
+        String actualString = matchRecord.toString();
+        String expectedString = username1 + " " + username2 + " " + gameType1 + " " + winner1 + " " + date1;
+        assertEquals(expectedString, actualString, "The string should be " + expectedString);
     }
 }
