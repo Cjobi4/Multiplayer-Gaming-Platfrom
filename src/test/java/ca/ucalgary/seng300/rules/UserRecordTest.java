a383c62b7e6dc2801578e2b9e84685ed98d8f7d1
Jaspreet Sandhu <jaspreet.sandhu3@ucalgary.ca>
2026-04-05 21:25:33 -0600

    Created UserRecordTest, in progress (#164)

---
+++ src/test/java/ca/ucalgary/seng300/rules/UserRecordTest.java
@@ -0,0 +1,32 @@
+package ca.ucalgary.seng300.rules;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRecordTest {
+
+    @Test
+    void testGetWinsTTT() {
+    }
+
+    @Test
+    void testGetWinsC4() {
+    }
+
+    @Test
+    void testGetMatchesTTT() {
+    }
+
+    @Test
+    void testGetMatchesC4() {
+    }
+
+    @Test
+    void testGetTotalWins() {
+    }
+
+    @Test
+    void testGetTotalMatches() {
+    }
+}
