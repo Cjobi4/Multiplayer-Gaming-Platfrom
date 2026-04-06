e4b1643f8de37b5d4c78118415cdfe6acd51a671
Owen <owen.hilton@ucalgary.ca>
2026-04-06 13:03:27 -0600

    added ActivePlayer class #268

---
+++ src/main/java/ca/ucalgary/seng300/shared/models/ActivePlayer.java
@@ -0,0 +1,18 @@
+package ca.ucalgary.seng300.shared.models;
+
+public class ActivePlayer {
+
+    private final String name;
+
+
+    public ActivePlayer(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+}
