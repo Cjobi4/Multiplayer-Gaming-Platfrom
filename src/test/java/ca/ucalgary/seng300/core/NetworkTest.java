d49615d97da6620dc432484925ed8e25f59f12e6
Jordan <jordan.tran2@ucalgary.ca>
2026-04-07 10:03:33 -0600

    Updated staging branch to reflect most changes

--- src/test/java/ca/ucalgary/seng300/core/NetworkTest.java
+++ src/test/java/ca/ucalgary/seng300/core/NetworkTest.java
@@ -16,6 +16,7 @@ public class NetworkTest {
 
     /** TODO rewrite tests to reflect updates in main code
      * Should simply require making a testing constructor in Network.java
+     * Theoretically no impact on the project itself
      */
     /*
     Most of these test cases can only test the logic of the functions
