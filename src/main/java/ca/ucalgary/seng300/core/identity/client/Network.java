9132fd488c31fa588909d1c8e18fff6f391da58a
Sanmeet <sanmeet.braich@ucalgary.ca>
2026-04-08 11:12:39 -0600

    [Platform] Updated TTT move sequence

--- src/main/java/ca/ucalgary/seng300/core/identity/client/Network.java
+++ src/main/java/ca/ucalgary/seng300/core/identity/client/Network.java
@@ -64,11 +64,15 @@ public class Network extends Thread {
     public static final byte KICKED_FROM_QUEUE = 13;
     public static final byte MATCH_ACCEPTED = 14;
     public static final byte MATCH_REJECTED = 15;
-    //??
-
-    public static final byte RESPOND_QUEUE = 35;
     public static final byte SEND_CHALLENGE = 16;
     public static final byte RECEIVE_CHALLENGE = 17;
+    private final int SEND_BOARD = 18;
+    private final int PROMPT_MOVE = 19;
+    private final int NOTIFY_GAME_STATE = 20;
+    private final int SEND_CHAT = 21;
+    private final int RECEIVE_CHAT = 22;
+
+    public static final byte RESPOND_QUEUE = 35;
 
     // to be added/modified later
     public static final byte SEND_MOVE_TTT = 125;
@@ -246,10 +250,14 @@ public class Network extends Thread {
                         receiveChallenge();
                     }
 //                    // TODO REMOVE THIS AFTER SERVER SIDE TURNS IMPLEMENTED
-//                    else if (descriptionByte == 19) {
-//                        System.out.println("Server is waiting for a move... Auto-skipping to unblock server!");
-//                        sendRequestParameter("dummy_local_move");
-//                    }
+                    else if (descriptionByte == PROMPT_MOVE) {
+                        System.out.println("Server is waiting for a move... Auto-skipping to unblock server!");
+                        sendRequestParameter("dummy_local_move");
+                    }
+                    else if (descriptionByte == SEND_BOARD) {
+                        String updatedBoard = receiveBoard();
+                        // TODO call something in UI and pass in updated board
+                    }
                 }
 
                 catch (SocketTimeoutException e) {
@@ -691,17 +699,18 @@ public class Network extends Thread {
         }
     }
 
-    public void sendMoveTTT(String boardState) throws Exception {
-        // send description byte
-        socket.getOutputStream().write(SEND_MOVE_TTT);
-
-        // send board
-        sendRequestParameter(boardState);
+    public void sendMoveTTT(String move) throws Exception {
+        // send board move
+        sendRequestParameter(move);
     }
 
-    public String receiveMoveTTT() throws Exception {
+    public String receiveBoard() throws Exception {
+        // read the updated ttt board
+        return readResponseString();
+    }
 
-        return null;
+    public void notifyPlayerTurn() throws Exception {
+        // TODO: ui updates to enable input for move, then call sendMoveTTT()
     }
 
     // LEADERBOARD
